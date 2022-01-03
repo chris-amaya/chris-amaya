3f2ecffb3fa64f320b494bebdcf990cc8c6af4f6
Christian Eduardo Amaya Rivas <amayarivasc@gmail.com>
2022-01-03 21:12:49 +0000

    fixed(article): broken variables
    some variables were breaking due changes in entities

--- packages/portfolio/src/pages/Article.tsx
+++ packages/portfolio/src/pages/Article.tsx
@@ -1,36 +1,22 @@
 import React, {useEffect, useState} from 'react'
 import ReactMarkdown from 'react-markdown'
 import remarkGfm from 'remark-gfm'
-
 import {useParams} from 'react-router-dom'
 
 import {IProject} from '@personal/common'
 import {Header} from '../components'
 import {useTranslation} from 'react-i18next'
 
-interface state extends IProject {
-  content: string
-}
-
 function Article() {
-  const [state, setState] = useState<Partial<state>>()
+  const [state, setState] = useState<IProject>()
   const {i18n} = useTranslation()
   const {id} = useParams()
 
   useEffect(() => {
     fetch(`http://localhost:8000/project/${id}`)
       .then((res) => res.json())
-      .then((data) =>
-        setState({...data.project, content: data.project.text[i18n.language]}),
-      )
+      .then((data) => setState(data.project as IProject))
   }, [])
-
-  useEffect(() => {
-    setState((prevState) => ({
-      ...prevState,
-      content: state?.text ? state.text[i18n.language] : '',
-    }))
-  }, [i18n.language])
 
   return (
     <>
@@ -38,12 +24,12 @@ function Article() {
         <Header />
       </div>
       <div className="hero-img">
-        <img src={state?.image} alt="" />
+        <img src={state?.images.hero} alt="" />
       </div>
       <section>
         <div className="container">
           <ReactMarkdown
-            children={state?.content || ''}
+            children={state?.lang[i18n.language].text || ''}
             remarkPlugins={[remarkGfm]}
           />
         </div>
