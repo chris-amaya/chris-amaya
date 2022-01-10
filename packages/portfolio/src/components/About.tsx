2bc23797aced6f1b8a6e95378d186388bbf7b14a
Christian Eduardo Amaya Rivas <amayarivasc@gmail.com>
2022-01-10 09:52:22 +0000

    add(i18n): about section

--- packages/portfolio/src/components/About.tsx
+++ packages/portfolio/src/components/About.tsx
@@ -12,15 +12,66 @@ export function About() {
             <h1>{t('about.title')}</h1>
           </div>
           <div className="about-me-content">
+            <p>{t('about.p1')}</p>
+            <p>{t('about.p2.pre')}</p>
+            <ul>
+              <li>
+                <Trans i18nKey={'about.p4.list.i1'}>
+                  <strong>More than 30 small personal projects</strong>:
+                  projects to enforce my knowledge while I was forming myself.
+                </Trans>
+              </li>
+              <li>
+                <Trans i18nKey={'about.p4.list.i2'}>
+                  <strong>1 personal project</strong>: comport is a project that
+                  I hope some day soon will be consumed.
+                </Trans>
+              </li>
+              <li>
+                <Trans i18nKey={'about.p4.list.i3'}>
+                  <strong>At least 3 big professional projects</strong>: while I
+                  was working at Quickapps I help finishing 2 big projects and
+                  one more left it on standby.
+                </Trans>
+              </li>
+            </ul>
+            <p>{t('about.p3')}</p>
+            <p>{t('about.p4.pre')}</p>
+            <ul>
+              <li>
+                <Trans i18nKey={'about.p4.list.i1'}>
+                  <strong>Fluent</strong>: HTML, CSS, JavaScript, Typescript,
+                  Vue, Node.js
+                </Trans>
+              </li>
+              <li>
+                <Trans i18nKey={'about.p4.list.i2'}>
+                  <strong>Proficient</strong>: React, Angular, Linux, Docker,
+                  MongoDB, MySQL, TDD/Jest, Git, Socket.io
+                </Trans>
+              </li>
+              <li>
+                <Trans i18nKey={'about.p4.list.i3'}>
+                  <strong>Prior Experience</strong>: Zoho, Genexus, D3.js, React
+                  Native
+                </Trans>
+              </li>
+            </ul>
             <p>
+              {t('about.p5.pre')}
               <Trans
-                i18nKey="about.text"
+                i18nKey={'about.p5.cert'}
                 components={{
                   cert: (
                     <a
                       target={'_blank'}
                       href="https://www.efset.org/cert/hpStKw"></a>
                   ),
+                }}
+              />
+              <Trans
+                i18nKey={'about.p5.efset'}
+                components={{
                   efset: (
                     <a target={'_blank'} href="https://www.efset.org/"></a>
                   ),
--- packages/portfolio/src/hooks/useTheme.ts
+++ packages/portfolio/src/hooks/useTheme.ts
@@ -10,12 +10,22 @@ export default function useTheme() {
       document.documentElement.style.setProperty('--color-dark', '#000')
       document.documentElement.style.setProperty('--color-text', '#fff')
       document.documentElement.style.setProperty('--color-accent', '#ffffff')
+      document.documentElement.style.setProperty('--color-rowTable', '#1b3954')
+      document.documentElement.style.setProperty(
+        '--color-headerTable',
+        '#01263a',
+      )
     } else if (theme === 'light') {
       document.documentElement.style.setProperty('--color-primary', '#01263a')
       document.documentElement.style.setProperty('--color-light', '#ffffff')
       document.documentElement.style.setProperty('--color-dark', '#000')
       document.documentElement.style.setProperty('--color-text', '#ffffff')
+      document.documentElement.style.setProperty('--color-rowTable', '#fff')
       document.documentElement.style.setProperty('--color-accent', '#01263a')
+      document.documentElement.style.setProperty(
+        '--color-headerTable',
+        '#f0f0f0',
+      )
     }
   }, [theme])
 
